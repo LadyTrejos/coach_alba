cd05b7cbcf839d0490568389f08da524bd7d4285
ivanvlizarazo <ivanlizarazo1305@gmail.com>
2020-01-31 09:20:46 -0500

    primer commit

---
+++ pages/Index.js
@@ -0,0 +1,14 @@
+import Layout from '../comps/MyLayout';
+
+export default function Index() {
+  return (
+    <div>
+        <Layout>
+            <p>Hello Ivan</p>
+        </Layout>
+        <Layout>
+            <p>This is the about page</p>
+        </Layout>
+    </div>
+  );
+}
