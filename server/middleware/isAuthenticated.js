34579e33dc579514a2cbb4faa5f998619433e706
ivanvlizarazo <ivanlizarazo1305@gmail.com>
2020-03-12 12:43:14 -0500

    se borra la carpeta server, de express

--- server/middleware/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.user) if (req.user.authenticated) return next();
-
-  res.status(403).send({ message: "Unauthorized." });
-}
-
-module.exports = isAuthenticated;
